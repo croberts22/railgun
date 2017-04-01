03e681344f55afc0a2f591e6defbe65ffff4326c
Corey Roberts <croberts@underarmour.com>
2017-04-01 10:14:36 -0500

    Moving rank types into its own method.

--- scrapers/anime_list_scraper.rb
+++ scrapers/anime_list_scraper.rb
@@ -21,31 +21,7 @@ module Railgun
       end_date = parse_end_date(nokogiri)
       member_count = parse_member_count(nokogiri)
 
-      rank_type = ''
-
-      # Available options: 'all', 'airing', 'upcoming', 'tv', 'movie', 'ova', 'special', 'popular', 'favorite'.
-      case list_type
-        when 'all'
-          rank_type = 'rank'
-        when 'airing'
-          rank_type = 'airing_rank'
-        when 'upcoming'
-          rank_type = 'upcoming_rank'
-        when 'tv'
-          rank_type = 'tv_rank'
-        when 'movie'
-          rank_type = 'movie_rank'
-        when 'ova'
-          rank_type = 'ova_rank'
-        when 'special'
-          rank_type = 'special_rank'
-        when 'popular'
-          rank_type = 'popularity_rank'
-        when 'favorite'
-          rank_type = 'favorited_rank'
-        else
-
-      end
+      rank_type = rank_key(list_type)
 
       {
           id: id,
@@ -63,6 +39,39 @@ module Railgun
               member_count: member_count
           }
       }
+
+    end
+
+    # Takes a list type param and converts it to the appropriate
+    # JSON key. This is a _safe_ method in that it will default
+    # to 'rank' if the list_type does not match any appropriate
+    # key (however, this should have already been guarded before the request,
+    # so you must have _really_ messed up if you got to this state).
+    def rank_key(list_type)
+
+      # Available options: 'all', 'airing', 'upcoming', 'tv', 'movie', 'ova', 'special', 'popular', 'favorite'.
+      case list_type
+        when 'all'
+          'rank'
+        when 'airing'
+          'airing_rank'
+        when 'upcoming'
+          'upcoming_rank'
+        when 'tv'
+          'tv_rank'
+        when 'movie'
+          'movie_rank'
+        when 'ova'
+          'ova_rank'
+        when 'special'
+          'special_rank'
+        when 'popular'
+          'popularity_rank'
+        when 'favorite'
+          'favorited_rank'
+        else
+          'rank'
+      end
 
     end
 
